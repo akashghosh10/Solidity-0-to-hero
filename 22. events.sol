29b31e659583099fccdc552fa2688dc7b24437f4
akashghosh10 <69781034+akashghosh10@users.noreply.github.com>
2024-01-23 21:58:30 +0530

    Updated the comments on events

--- 22. events.sol
+++ 22. events.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.19;
 
 contract Event {
 
-    //An event is declared when we need to store something in the blockchain permamnently, but do not retrieve it.
+    //An event is declared when we need to store something in the blockchain permamnently, i.e. log it.
+    //However, we won't be able to retrieve it from the blockchain, but we can only search for it using indexed parametersand get what was emitted, as in logged.
     
-    // Indexed parameters helps you filter the logs by the indexed parameter
+    // Indexed parameters helps you filter the logs by the indexed parameter and search for them
     event indexedLog(address indexed sender, string message);
     //NOTE - Up to 3 parameters can be indexed in an event declaration.
     
