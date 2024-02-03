c2772a98d8e84e587767ab9fed9f8683e9461680
akashghosh10 <69781034+akashghosh10@users.noreply.github.com>
2024-02-03 23:30:29 +0530

    added new comments

--- 34. keccack256.sol
+++ 34. keccack256.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+//keccack256  is used for signing and protecting against a wide range of
+//attacks, including preimage attacks, collision attacks,
+//and length extension attacks. It is popularly used in solidity.
+
 contract HashFunction {
     function hash(string memory _text, uint _num, address _addr) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_text, _num, _addr)); //use abi.encodePacked() to convert the input into bytes32
+        return keccak256(abi.encodePacked(_text, _num, _addr)); //use abi.encodePacked() to convert the input into bytes32.
+        //Keccack256() returns the hash of the given inputs.
     }
 
     //Hash collision is when 2 different inputs generate the same hash for the same hashing algorithm
     //encode will return bytes
-    //encodePacked will return bytes in a compressed form
+    //encodePacked will return bytes in a compressed form(bytes32)
     // Hash collision can occur when you pass more than one dynamic data type
     // to abi.encodePacked. In such case, you should use abi.encode instead.
     function collision(
@@ -22,13 +27,15 @@ contract HashFunction {
     }
 }
 
+//This is a simple game-like-contract to illustrate that the hash
+//for an input is unique, and is pretty difficult to reverse engineer
 contract GuessTheMagicWord {
     bytes32 public answer =
         0x60298f78cc0b47170ba79c10aa3851d7648bd96f2f8e46a19dbc777c36fb0c00;
 
     // Magic word is "Solidity"
     function guess(string memory _word) public view returns (bool) {
-        return keccak256(abi.encodePacked(_word)) == answer;
+        return keccak256(abi.encodePacked(_word)) == answer; //statement checking if the hash of the input string is same as the hash stored in answer
     }
 }
 
